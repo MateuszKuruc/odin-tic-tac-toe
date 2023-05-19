69b29af0a56a3cc5571a1730ae4e913077604d7d
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-05-19 14:59:53 +0200

    add nested loop to create DOM elements for gameboard grid

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 "use strict";
 
-const gameBoard = {
-  gameboard: [],
-};
+const gameBoard = (function() {
+    const boardRows = 3;
+    const boardColumns = 3;
+    const container = document.querySelector('.gameboard');
+    for (let i = 0; i < boardRows; i++) {
+        const rowCell = document.createElement('div');
+        rowCell.classList.add('boardCell');
+        container.appendChild(rowCell);
+        for (let j = 0; j < boardColumns -1; j++) {
+            const columnCell = document.createElement('div');
+            columnCell.classList.add('boardCell');
+            container.appendChild(columnCell);
+        }
+    }
+})();
 
 function FactoryPlayer(name, symbol) {
   return { name, symbol };
