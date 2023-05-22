8de9f9b4661080d61e6c153c3bb05634568bd64f
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-05-22 11:26:15 +0200

    improve restart game button and winner message

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 "use strict";
 
 // let activePlayer = "X";
-let player1;
-let player2;
+// let player1;
+// let player2;
 let winner;
 
 const gameBoard = (function () {
   let gameboard = ["", "", "", "", "", "", "", "", ""];
   let activePlayer = 'X';
   const fieldElements = document.querySelectorAll(".field");
+  const resultMessage = document.querySelector('#resultMessage');
 
   const updateGameboard = (function () {
     for (let i = 0; i < gameboard.length; i++) {
         const fieldElement = document.querySelector(`#n${i}`);
-        fieldElement.addEventListener('click', function() {
-            if (activePlayer === 'X' && this.innerHTML === '') {
+        const symbolInput = function() {
+            if (winner === 'player1' || winner === 'player2') return;
+            else if (activePlayer === 'X' && this.innerHTML === '') {
                 this.innerHTML = 'X';
                 gameboard[i] = this.innerHTML;
                 activePlayer = 'O';
@@ -25,12 +27,13 @@ const gameBoard = (function () {
                 activePlayer = 'X';
                 checkWinner();
             }
-        })
+        }
+        fieldElement.addEventListener('click', symbolInput)
     }
   })()
 
   const checkWinner = function() {
-
+    
     if (
         (gameboard[0] === 'X' && gameboard[1] === 'X' && gameboard[2] === 'X') || 
         (gameboard[3] === 'X' && gameboard[4] === 'X' && gameboard[5] === 'X') || 
@@ -39,6 +42,7 @@ const gameBoard = (function () {
         (gameboard[2] === 'X' && gameboard[4] === 'X' && gameboard[6] === 'X')
     ) {
         console.log('Player1 is the winner!');
+        resultMessage.innerHTML = ('Player1 is the winner!');
         winner = 'player1';
     }
     else if (
@@ -49,11 +53,13 @@ const gameBoard = (function () {
         (gameboard[2] === 'O' && gameboard[4] === 'O' && gameboard[6] === 'O')
     ) {
         console.log('Player2 is the winner!');
+        resultMessage.innerHTML = ('Player2 is the winner!');
         winner = 'player2';
     }   else if ((gameboard[0] != '' && gameboard[1] != '' && gameboard[2] != '' && gameboard[3] != '' &&
         gameboard[4] != '' && gameboard[5] != '' && gameboard[6] != '' && gameboard[7] != '' &&
         gameboard[8] != '') && (winner != 'player1' && winner != 'player2')) {
         console.log('It is a draw!');
+        resultMessage.innerHTML = ('It is a draw!')
     }
   }
 
@@ -65,25 +71,6 @@ const gameBoard = (function () {
   }
 })();
 
-// create players function - come back later
-
-// const createPlayers = (function() {
-//     const buttonX = document.querySelector('#playerX');
-//     const buttonO = document.querySelector('#playerO');
-//     let player1;
-//     let player2;
-//     buttonX.addEventListener('click', function() {
-//         player1 = FactoryPlayer('Player_1', 'X');
-//         console.log(player1);
-//         // return player1;
-//     });
-//     buttonO.addEventListener('click', function() {
-//         player2 = FactoryPlayer('Player_2', 'O');
-//         console.log(player2);
-//         // return player2;
-//     })
-//     return { player1, player2 }
-// })();
 
 function FactoryPlayer(name, symbol) {
   return { name, symbol };
@@ -105,8 +92,17 @@ const gameFlow = (function() {
     restartButton.addEventListener('click', restartGame);
 })();
 
+const createPlayers = (function() {
+    const buttonX = document.querySelector('#playerX');
+    const buttonO = document.querySelector('#playerO');
+    // const player1Name = prompt('choose your name');
+    // const player1 = FactoryPlayer(player1Name, 'X');
+    // console.log(player1);
+})();
+
+
 const displayController = (function () {
-//   gameBoard.updateGameboard();
+
 
 
 })();
