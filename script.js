4f1d582319e9c7136a93c707d18fd8591a2d0fe3
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-05-22 13:31:12 +0200

    fix player factory function to create both players with input form

--- script.js
+++ script.js
@@ -4,36 +4,84 @@
 // let player1;
 // let player2;
 let winner;
+let player1;
+let player2;
+let activePlayer = player1;
+
+
+const createPlayers = (function() {
+    const buttonX = document.querySelector('#playerX');
+    const buttonO = document.querySelector('#playerO');
+    const confirmButton = document.querySelector('#submit');
+    const formBlock = document.querySelector('.form-block')
+    let player1Name;
+    let player2Name;
+  
+   
+    const createPlayer1 = function() {
+    confirmButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        player1Name = document.querySelector('#name').value;
+        player1 = FactoryPlayer(player1Name, 'X');
+        console.log(player1);
+        formBlock.classList.add('hidden');
+    }) 
+    }
+
+    const createPlayer2 = function() {
+        confirmButton.addEventListener('click', function(event) {
+            event.preventDefault();
+            player2Name = document.querySelector('#name').value;
+            player2 = FactoryPlayer(player2Name, 'O');
+            console.log(player2);
+            formBlock.classList.add('hidden');
+        })
+    }
+
+    buttonX.addEventListener('click', function() {
+        formBlock.classList.remove('hidden');
+        createPlayer1();
+    })
+
+    buttonO.addEventListener('click', function() {
+        formBlock.classList.remove('hidden');
+        createPlayer2();
+    })
+    return { player1, player2 }
+})();
 
 const gameBoard = (function () {
   let gameboard = ["", "", "", "", "", "", "", "", ""];
-  let activePlayer = 'X';
   const fieldElements = document.querySelectorAll(".field");
   const resultMessage = document.querySelector('#resultMessage');
 
-  const updateGameboard = (function () {
+  const updateGameboard = (function() {
     for (let i = 0; i < gameboard.length; i++) {
         const fieldElement = document.querySelector(`#n${i}`);
         const symbolInput = function() {
-            if (winner === 'player1' || winner === 'player2') return;
-            else if (activePlayer === 'X' && this.innerHTML === '') {
+            // if (player1 === '' || player1 === undefined && player2 === '' || player2 === undefined) return;
+            if (winner === player1 || winner === player2) return;
+            // chat gpt
+            if (this.innerHTML != '') return;
+            // chat gpt
+            if (activePlayer === player1 && this.innerHTML === '') {
                 this.innerHTML = 'X';
                 gameboard[i] = this.innerHTML;
-                activePlayer = 'O';
+                activePlayer = player2;
                 checkWinner();
-            }   else if (activePlayer === 'O' && this.innerHTML === '') {
+            }   if (activePlayer === player2 && this.innerHTML === '') {
                 this.innerHTML = 'O';
                 gameboard[i] = this.innerHTML;
-                activePlayer = 'X';
+                activePlayer = player1;
                 checkWinner();
             }
-        }
+
+    }
         fieldElement.addEventListener('click', symbolInput)
     }
-  })()
+  })();
 
   const checkWinner = function() {
-    
     if (
         (gameboard[0] === 'X' && gameboard[1] === 'X' && gameboard[2] === 'X') || 
         (gameboard[3] === 'X' && gameboard[4] === 'X' && gameboard[5] === 'X') || 
@@ -92,13 +140,7 @@ const gameFlow = (function() {
     restartButton.addEventListener('click', restartGame);
 })();
 
-const createPlayers = (function() {
-    const buttonX = document.querySelector('#playerX');
-    const buttonO = document.querySelector('#playerO');
-    // const player1Name = prompt('choose your name');
-    // const player1 = FactoryPlayer(player1Name, 'X');
-    // console.log(player1);
-})();
+
 
 
 const displayController = (function () {
