85aca1a3a223c968d26e3afc279328a1806f1f77
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-05-22 21:37:33 +0200

    restructure gameboard and game functions

--- script.js
+++ script.js
@@ -1,53 +1,40 @@
 "use strict";
 
-// let activePlayer = "X";
-// let player1;
-// let player2;
-let winner;
-let player1;
-let player2;
-let activePlayer = player1;
-
-
-const createPlayers = (function() {
-    const buttonX = document.querySelector('#playerX');
-    const buttonO = document.querySelector('#playerO');
+const game = (function() {
+    const startGame = document.querySelector('#startGame');
     const confirmButton = document.querySelector('#submit');
     const formBlock = document.querySelector('.form-block')
     let player1Name;
     let player2Name;
-  
+    let activePlayer;
+    let player1;
+    let player2;
+    let winner;
    
-    const createPlayer1 = function() {
+    const createPlayer = function() {
     confirmButton.addEventListener('click', function(event) {
         event.preventDefault();
         player1Name = document.querySelector('#name').value;
-        player1 = FactoryPlayer(player1Name, 'X');
-        console.log(player1);
+        player2Name = document.querySelector('#name2').value;
+        game.player1 = FactoryPlayer(player1Name, 'X');
+        game.player2 = FactoryPlayer(player2Name, 'O');
+        // console.log(player1, player2);
         formBlock.classList.add('hidden');
+        game.activePlayer = game.player1;
     }) 
     }
 
-    const createPlayer2 = function() {
-        confirmButton.addEventListener('click', function(event) {
-            event.preventDefault();
-            player2Name = document.querySelector('#name').value;
-            player2 = FactoryPlayer(player2Name, 'O');
-            console.log(player2);
-            formBlock.classList.add('hidden');
-        })
+    startGame.addEventListener('click', function() {
+        formBlock.classList.remove('hidden');
+        createPlayer();
+    })
+
+    return {
+        activePlayer,
+        player1,
+        player2,
+        winner
     }
-
-    buttonX.addEventListener('click', function() {
-        formBlock.classList.remove('hidden');
-        createPlayer1();
-    })
-
-    buttonO.addEventListener('click', function() {
-        formBlock.classList.remove('hidden');
-        createPlayer2();
-    })
-    return { player1, player2 }
 })();
 
 const gameBoard = (function () {
@@ -59,23 +46,18 @@ const gameBoard = (function () {
     for (let i = 0; i < gameboard.length; i++) {
         const fieldElement = document.querySelector(`#n${i}`);
         const symbolInput = function() {
-            // if (player1 === '' || player1 === undefined && player2 === '' || player2 === undefined) return;
-            if (winner === player1 || winner === player2) return;
-            // chat gpt
+            if (game.winner === game.player1 || game.winner === game.player2) return;
             if (this.innerHTML != '') return;
-            // chat gpt
-            if (activePlayer === player1 && this.innerHTML === '') {
+            if (game.activePlayer === game.player1 && this.innerHTML === '') {
                 this.innerHTML = 'X';
                 gameboard[i] = this.innerHTML;
-                activePlayer = player2;
-                checkWinner();
-            }   if (activePlayer === player2 && this.innerHTML === '') {
+                game.activePlayer = game.player2;
+            }   if (game.activePlayer === game.player2 && this.innerHTML === '') {
                 this.innerHTML = 'O';
                 gameboard[i] = this.innerHTML;
-                activePlayer = player1;
-                checkWinner();
+                game.activePlayer = game.player1;
             }
-
+            checkWinner();
     }
         fieldElement.addEventListener('click', symbolInput)
     }
@@ -87,22 +69,28 @@ const gameBoard = (function () {
         (gameboard[3] === 'X' && gameboard[4] === 'X' && gameboard[5] === 'X') || 
         (gameboard[6] === 'X' && gameboard[7] === 'X' && gameboard[8] === 'X') ||
         (gameboard[0] === 'X' && gameboard[4] === 'X' && gameboard[8] === 'X') ||
+        (gameboard[0] === 'X' && gameboard[3] === 'X' && gameboard[6] === 'X') ||
+        (gameboard[1] === 'X' && gameboard[4] === 'X' && gameboard[7] === 'X') ||
+        (gameboard[2] === 'X' && gameboard[5] === 'X' && gameboard[8] === 'X') ||
         (gameboard[2] === 'X' && gameboard[4] === 'X' && gameboard[6] === 'X')
     ) {
         console.log('Player1 is the winner!');
-        resultMessage.innerHTML = ('Player1 is the winner!');
-        winner = 'player1';
+        resultMessage.innerHTML = (`${game.player1.name} is the winner!`);
+        winner = game.player1;
     }
     else if (
         (gameboard[0] === 'O' && gameboard[1] === 'O' && gameboard[2] === 'O') || 
         (gameboard[3] === 'O' && gameboard[4] === 'O' && gameboard[5] === 'O') || 
         (gameboard[6] === 'O' && gameboard[7] === 'O' && gameboard[8] === 'O') ||
         (gameboard[0] === 'O' && gameboard[4] === 'O' && gameboard[8] === 'O') ||
+        (gameboard[0] === 'O' && gameboard[3] === 'O' && gameboard[6] === 'O') ||
+        (gameboard[1] === 'O' && gameboard[4] === 'O' && gameboard[7] === 'O') ||
+        (gameboard[2] === 'O' && gameboard[5] === 'O' && gameboard[8] === 'O') ||
         (gameboard[2] === 'O' && gameboard[4] === 'O' && gameboard[6] === 'O')
     ) {
         console.log('Player2 is the winner!');
-        resultMessage.innerHTML = ('Player2 is the winner!');
-        winner = 'player2';
+        resultMessage.innerHTML = (`${game.player2.name} is the winner!`);
+        winner = game.player2;
     }   else if ((gameboard[0] != '' && gameboard[1] != '' && gameboard[2] != '' && gameboard[3] != '' &&
         gameboard[4] != '' && gameboard[5] != '' && gameboard[6] != '' && gameboard[7] != '' &&
         gameboard[8] != '') && (winner != 'player1' && winner != 'player2')) {
@@ -115,7 +103,6 @@ const gameBoard = (function () {
     updateGameboard,
     gameboard,
     fieldElements,
-    // checkWinner
   }
 })();
 
@@ -124,14 +111,14 @@ function FactoryPlayer(name, symbol) {
   return { name, symbol };
 }
 
-const gameFlow = (function() {
+
+const restartGame = (function() {
    const restartButton = document.querySelector('#restart');
-    const resultMessage = document.querySelector('#resultMessage');
     
     const restartGame = function() {
         gameBoard.gameboard = ['', '', '', '', '', '', '', '', ''];
         resultMessage.innerHTML = '';
-        winner = '';
+        game.winner = '';
         for (let i = 0; i < gameBoard.fieldElements.length; i++) {
             const fieldElement = document.querySelector(`#n${i}`);
             fieldElement.innerHTML = '';
@@ -139,13 +126,3 @@ const gameFlow = (function() {
     }
     restartButton.addEventListener('click', restartGame);
 })();
-
-
-
-
-const displayController = (function () {
-
-
-
-})();
-
